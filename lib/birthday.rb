0e1144f362e622e454ce4922f46f6fdc356b0616
Lauren Stone <lstone321@gmail.com>
2020-07-19 01:42:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each {|kids_name| age >= 12}
-
+end
   puts "Happy Birthday #{name}! You are now #{age} years old! You are too old for this."
 end 
 end
